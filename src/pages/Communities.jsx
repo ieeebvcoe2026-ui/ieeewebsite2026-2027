f47eb5f9bebf9b334769a3ce101b6be6f6741827
aaru <aaru@123gmail.com>
2026-04-05 17:03:38 +0530

    reoved unwanted and sig completed

--- src/pages/Communities.jsx
+++ src/pages/Communities.jsx
@@ -17,11 +17,11 @@ import LogoBQC from "../assets/Communities/bqc.png";
 import LogoGamma from "../assets/Communities/gamma.png";
 import LogoDrishti from "../assets/Communities/drishti.png";
 import SigCard from "../components/SigCard";
-import CodeX from "../assets/Communities/bgcodexx.jpeg";
-import Gamma from "../assets/Communities/bggammaa.jpeg";
+import CodeX from "../assets/Communities/bgcodexx.png";
+import Gamma from "../assets/Communities/bggammaa.png";
 import Bqc from "../assets/Communities/bgbqcc.png";
-import Ecell from "../assets/Communities/bgecelll.jpeg";
-import Drishti from "../assets/Communities/bgdrishtii.jpeg"
+import Ecell from "../assets/Communities/bgecelll.png";
+import Drishti from "../assets/Communities/bgdrishtii.png"
 
 const subchapters = [
   {
